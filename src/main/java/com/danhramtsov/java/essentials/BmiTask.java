3e6c1bbe4cc78a0fd5705da77bd074c48e1d0cdf
Ale mae <deadun99@gmail.com>
2025-10-18 11:58:28 +0300

    1

--- src/main/java/com/danhramtsov/java/essentials/BmiTask.java
+++ src/main/java/com/danhramtsov/java/essentials/BmiTask.java
@@ -1,7 +1,20 @@
 package com.danhramtsov.java.essentials;
 
 
+import java.util.Scanner;
+
 public class BmiTask {
     //TODO: реализуйте код ниже
+            public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Введите ваш вес (в кг): ");
+            String name = sc.nextLine();
+            System.out.print("Введите ваш рост (в метрах): ");
+            double  = sc.nextInt();
+            sc.nextLine();
+            weight =
+            System.out.println("Ваш индекс массы тела (ИМТ) " + age);
+            System.out.println("Город: " + city);
+            sc.close();
 
 }
